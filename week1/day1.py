de362931d5383b069790977406b6bc0645a5af02
Pradeep Kasipuri <datawithpradeeptelugu@gmail.com>
2026-08-27 07:18:22 +0530

    Day 1 :variables ,print,datatypes

--- week1/day1.py
+++ week1/day1.py
@@ -1,1 +1,69 @@
 print('hello DE')
+
+
+x=1
+
+print(x)
+
+x=2
+
+print(x)
+
+
+y=x+3
+print(y)
+
+print("My name is Pradeep")
+print("Pradeep Learning Python")
+print("Pradeep Wants to become a Python expert")
+
+name="Navyaa"
+print("My name is",name)
+print(name,"Learning Python")
+print(name,"Wants to become a Python expert")
+
+name="Navya"
+language="Python"
+print("My name is",name)
+print(name,"Learning",language)
+print(name,"Wants to become a", language, "expert")
+
+
+#print the following three lines
+#Add a varaiable to make it dynamic
+
+sld="datawithpradeep"
+tld="com"
+print("info@",sld,".",tld)
+print("support@",sld,".",tld)
+print("www.",sld,".",tld)
+
+
+# Data Types in Pyhton
+a=10 #int
+b=3.5 #float
+c="Pradeep" #str
+d='Hello' #str
+e="1234" #str
+f=True #bool
+g=False 
+h=None #NoneType
+i="" #Str-Blank
+j=" " #Str -Empty Space
+print(a)
+
+age=25;
+your_height=5.9;
+your_name="Pradeep";
+are_you_a_student=True;
+
+print("your age",age)
+print("your_height",your_height);
+print("your_name",your_name);
+print("are_you_a_student",are_you_a_student);
+
+
+print("hello Pradeep Kasipuri",1,"ILU",sep="$")
+
+print("""hello pradeep kasipuri
+how are you """)
