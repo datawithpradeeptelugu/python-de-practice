9b8b3a5d42b98ccd404812bcd2018e33410496d1
Pradeep Kasipuri <datawithpradeeptelugu@gmail.com>
2026-09-08 15:38:24 +0530

    Day 10:Function Basics

---
+++ week3/day9- functions.py
@@ -0,0 +1,74 @@
+'''Functions:
+A small,reusable block of code that does one specific job'''
+
+'''Why Functions
+
+You usullay write small logic or flow 
+
+Inorder to solve small problem
+
+Over the time you might face simillar problem
+
+Where the logic going to be the almost the same as previous one
+
+What you are going to do is the copy and paste
+
+At the start,it sounds really okay
+
+over the time you are going to find out that same logic exist multiple times in your code
+
+This is the start of the choas because of imagine you found a bug or something wrong
+
+inside the logic or may be the requirement change .
+
+so now things get really annoying becasue you have to remember
+
+all the places where you use the logic and then do the changes
+
+and believe me in real project you code going to be massive
+
+atleast you are going to miss one
+
+So some of the logics got updated and others using the old logic
+
+which is going to make the result of the code inaccurate.
+
+Lose:
+Inconsistent Behaviour
+Hard to change
+Time Consuming
+Hard to read
+Hard to understand
+
+
+Inorder to put same logic over and over again you are going to take your logic and put it in one place
+inside function
+
+And whenever you need this logic,this behaviour ,you just call the function
+
+So you just don't rewrite it 
+
+you don't copy and past it 
+
+Just usins same logic whenever you want.
+
+And by doing this actullay all the problems going to disapper
+
+So everything in one place
+
+If you find for example  a bug or something wrong in the logic
+
+or may be the requirement change is all what you have to do is 
+
+Just go to the function and do the change only once
+
+With every part of the program
+
+That is using it automatically  going to get the new behaviour 
+
+or the new updates
+
+
+
+
+'''
